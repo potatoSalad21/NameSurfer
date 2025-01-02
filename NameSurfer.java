26c5d8ea4ab3d53774504eecb017403f4d321808
potatoSalad21 <eeperpeepo@gmail.com>
2025-01-02 20:10:41 +0400

    initial commit

---
+++ NameSurfer.java
@@ -0,0 +1,30 @@
+/*
+ * File: NameSurfer.java
+ * ---------------------
+ * When it is finished, this program will implements the viewer for
+ * the baby-name database described in the assignment handout.
+ */
+
+import acm.program.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class NameSurfer extends ConsoleProgram implements NameSurferConstants {
+
+/* Method: init() */
+/**
+ * This method has the responsibility for reading in the data base
+ * and initializing the interactors at the bottom of the window.
+ */
+	public void init() {
+	}
+
+/* Method: actionPerformed(e) */
+/**
+ * This class is responsible for detecting when the buttons are
+ * clicked, so you will have to define a method to respond to
+ * button actions.
+ */
+	public void actionPerformed(ActionEvent e) {
+	}
+}
---
+++ NameSurferDataBase.java
@@ -0,0 +1,25 @@
+public class NameSurferDataBase implements NameSurferConstants {
+
+/* Constructor: NameSurferDataBase(filename) */
+/**
+ * Creates a new NameSurferDataBase and initializes it using the
+ * data in the specified file.  The constructor throws an error
+ * exception if the requested file does not exist or if an error
+ * occurs as the file is being read.
+ */
+	public NameSurferDataBase(String filename) {
+		// You fill this in //
+	}
+
+/* Method: findEntry(name) */
+/**
+ * Returns the NameSurferEntry associated with this name, if one
+ * exists.  If the name does not appear in the database, this
+ * method returns null.
+ */
+	public NameSurferEntry findEntry(String name) {
+		// You need to turn this stub into a real implementation //
+		return null;
+	}
+}
+
